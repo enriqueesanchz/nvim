ac656b9cd0c97235bbb3f30e12f9fb2e80424c88
enriqueesanchz <enrsancar1@alum.us.es>
2024-03-27 21:25:22 +0100

    remove @property bg

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,6 +1,6 @@
 require('rose-pine').setup({
     highlight_groups = {
-        ['@variable'] = { italic = false }
+        ['@variable'] = { italic = false, bg = "none" }
     }
 })
 
@@ -10,6 +10,7 @@ function ColorMyPencils(color)
 	
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "@Property", { bg = "none" })
 end
 
 ColorMyPencils() 
